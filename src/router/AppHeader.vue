55ffee8aba618fa9cd92fa0738b873bf7d3b1d24
Jacky Lui <jacky@intelics.com.au>
2020-11-30 17:09:48 +1000

    unit testing

---
+++ src/router/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button v-show="loggedIn">Logout</button>
+    </div>
+</template>
+
+<script>
+export default {
+    //name: 'component-name',
+    //parent: '',
+    components: {},
+    directives: {},
+    filters: {},
+    mixins: [],
+    props: {},
+    data: function () 
+    { 
+        return {
+            loggedIn: false
+        } 
+    },
+    computed: {},
+    watch: {},
+    beforeCreate: function () {},
+    created: function () {},
+    beforeMount: function () {},
+    mounted: function () {},
+    beforeUpdate: function () {},
+    updated: function () {},
+    activated: function () {},
+    deactivated: function () {},
+    beforeDestroy: function () {},
+    destroyed: function () {},
+    methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
